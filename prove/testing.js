dcb05bf24b862b7f2582691853253327c4db4856
Nuttida Meeboon <nuttida.donut@mail.kmutt.ac.th>
2021-09-14 00:37:17 +0700

    add 14/9/2021

--- prove/testing.js
+++ prove/testing.js
@@ -1,12 +1,12 @@
 //for ซ้อน for
-var str = ''
-for (i = 1; i <= 3; i++) {
-    str = ''
-    for (j = 1; j <= 5; j++) {
-        str += j + ' '
-    }
-    console.log(str)
-}
+// var str = ''
+// for (i = 1; i <= 3; i++) {
+//     str = ''
+//     for (j = 1; j <= 5; j++) {
+//         str += j + ' '
+//     }
+//     console.log(str)
+// }
 
 // var str = ''
 // for (i = 1; i <= 5; i++) {
@@ -15,4 +15,20 @@ for (i = 1; i <= 3; i++) {
 //         str += j + ' '
 //     }
 //     console.log(str)
+// }
+
+
+// const prompt = require('promot-syn')()
+
+// function getInteger() {
+//     let value = prompt('Enter Integer Number >> ')
+//     return parseInt(value)
+// }
+
+// let x
+// while (true) {
+//     x = getInteger()
+//     if (!Number.isInteger(x)) {
+//         continue
+//     } else { break }
 // }
