6fb241563639b75acda155480c4a110912b0cb87
Nuttida Meeboon <nuttida.donut@mail.kmutt.ac.th>
2021-11-21 01:20:37 +0700

    add

---
+++ prove/array.js
@@ -0,0 +1,10 @@
+var friends = new Array('kaopoon', 'donut', 'view')
+
+console.log(friends.includes('donut'));
+console.log(friends.includes('do'));
+console.log(friends.sort());
+console.log(friends.reverse());
+friends.push('tui', 'nut')
+console.log(friends);
+friends.unshift('darin');
+console.log(friends);
---
+++ prove/forEach.js
@@ -0,0 +1,4 @@
+var friends = new Array('kaopoon', 'donut', 'view')
+friends.forEach(function(b, i) {
+    console.log(i + 1, b);
+})
